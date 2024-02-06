6da5051f026ecdea2280788356566a788dd411be
RheihanFR <rheyjr1234@gmail.com>
2024-02-07 01:38:03 +0700

    update regis

--- resources/views/customer/riwayat/transaksi.blade.php
+++ resources/views/customer/riwayat/transaksi.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
-@section('content')
 
+@section('content')
     <!-- main content area start -->
     <div class="content-wrapper">
         <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
@@ -15,7 +15,7 @@
                                     <div class="card-body">
                                         <h4 class="header-title">Riwayat Transaksi</h4>
                                         <div class="data-tables">
-                                            <table id="table1" class="table table-bordered table-hover">
+                                            <table id="transaction-table" class="table table-bordered table-hover">
                                                 <thead class="bg-light text-capitalize">
                                                     <tr>
                                                         <th class="col-1">No.</th>
@@ -35,15 +35,14 @@
                                                             <td>{{ $transaksi->harga }}</td>
                                                             <td>{{ $transaksi->kuantitas }}</td>
                                                             <td>{{ $transaksi->tgl_transaksi }}</td>
-                                                            <td>Rp.
-                                                                {{ number_format($transaksi->total_harga, 0, ',', '.') }},00</td>
+                                                            <td>Rp.{{ number_format($transaksi->total_harga, 0, ',', '.') }},00</td>
                                                             <td>{{ $transaksi->status }}</td>
                                                         </tr>
                                                     @endforeach
                                                 </tbody>
-
                                             </table>
                                         </div>
+                                        <button onclick="printTransactionHistory()" class="btn btn-primary mt-3">Cetak</button>
                                     </div>
                                 </div>
                             </div>
@@ -55,6 +54,16 @@
             </div>
         </div>
     </div>
-    </div>
     <!-- main content area end -->
+
+    <script>
+        function printTransactionHistory() {
+            // Mencetak tabel menggunakan JavaScript
+            var printContents = document.getElementById('transaction-table').outerHTML;
+            var originalContents = document.body.innerHTML;
+            document.body.innerHTML = printContents;
+            window.print();
+            document.body.innerHTML = originalContents;
+        }
+    </script>
 @endsection
